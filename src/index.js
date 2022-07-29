b8ec5d0c4838d4bdbd0b730b1fbe58806b4e4567
kmikhaliuto <kmihaliuto@mail.ru>
2022-07-30 00:06:11 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const numbersOnes = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const numbersTwoes = ['zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number < 20) {
+        return numbersOnes[number];
+    } else if (number < 100) {
+       let result = numbersTwoes[Math.floor(number / 10)] + ((number % 10 === 0) ? '' : ' ' +  numbersOnes[number % 10]);
+        return result;
+    } else if (number >= 100 && number <=999) {
+        result = numbersOnes[Math.floor(number / 100)] + ' hundred' + ((number % 100 === 0) ? '' : ' ' + toReadable([number % 100]))
+        return  result;
+    }
 }
